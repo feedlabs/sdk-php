e793895401926089ab828df06e25c9f2055d02e1
Marian Sollmann <ms@sollmann-interactive.com>
2014-09-02 19:12:41 +0200

    test

---
+++ source/feedlabs/nextproduct/Agent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace feedlabs\nextproduct;
+
+class Agent {
+
+    public function __construct() {
+
+    }
+
+    public function getClient() {
+
+    }
+
+    public function FeedPages(){
+        $listNew = array();
+        $list = getClient("feedpages");
+        foreach($list as $feedpage) {
+            $listNew[] = new Resource_FeedPage($feedpage);
+        }
+    }
+}
